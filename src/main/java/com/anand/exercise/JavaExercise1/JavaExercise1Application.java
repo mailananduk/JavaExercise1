72d29dd6e33f0aef70a5b4bc6a22e113d0cd891c
Anand Chetty <mailanand.k@gmail.com>
2022-01-29 22:17:39 +0000

    initial commit -mvn, springboot

---
+++ src/main/java/com/anand/exercise/JavaExercise1/JavaExercise1Application.java
@@ -0,0 +1,13 @@
+package com.anand.exercise.JavaExercise1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaExercise1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaExercise1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/anand/exercise/JavaExercise1/JavaExercise1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anand.exercise.JavaExercise1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaExercise1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
